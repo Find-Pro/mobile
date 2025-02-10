9f53a835c4484e15760116b31bf2baebdf106c57
ookokk <osmankok701@gmail.com>
2025-02-10 12:02:12 +0300

    messages app bar changed

--- lib/feature/message/view/messages_view.dart
+++ lib/feature/message/view/messages_view.dart
@@ -5,7 +5,7 @@ import 'package:findpro/common/widget/custom_circular.dart';
 import 'package:findpro/common/widget/no_data_widget.dart';
 import 'package:findpro/common/widget/you_should_login_app_widget.dart';
 import 'package:findpro/feature/home/widget/home_background_image.dart';
-import 'package:findpro/feature/home/widget/select_country_app_bar.dart';
+import 'package:findpro/feature/home/widget/home_country_app_bar.dart';
 import 'package:findpro/feature/message/view_model/messages_view_model.dart';
 import 'package:findpro/feature/message/widget/messages_user_card.dart';
 import 'package:findpro/feature/message/widget/messages_users_circle_avatar.dart';
@@ -27,7 +27,7 @@ class MessagesView extends ConsumerWidget {
       return const YouShouldLoginAppWidget();
     }
     return Scaffold(
-      appBar: SelectCountryAppBar(text: LocaleKeys.messages.tr()),
+      appBar: const HomeCountryAppBar(),
       body: messagesFuture.when(
         data: (_) {
           if (messagesViewModel.isEmpty) {
